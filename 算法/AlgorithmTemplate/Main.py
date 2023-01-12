7e5af25e2a1e7700a7ec94b2381576f785e3e61f
86159 <zxw3180716935@163.com>
2023-01-13 00:11:04 +0800

    add

---
+++ 算法/AlgorithmTemplate/Main.py
@@ -0,0 +1,24 @@
+'''  ---用于实验算法模板---  '''
+
+# 快排实验
+'''
+from 算法.AlgorithmTemplate import BasicAlgorithms
+
+n = int(input())
+lst1 = list(map(int, input().split()))
+
+BasicAlgorithms.quick_sort(lst1, 0, n - 1)
+
+print(" ".join(map(str,lst1)))
+'''
+
+# 快选实验
+
+from 算法.AlgorithmTemplate import BasicAlgorithms
+
+nk = list(map(int, input().split()))
+n = nk[0]
+k = nk[1]
+lst = list(map(int, input().split()))
+ans = BasicAlgorithms.quick_choose(lst,0,n-1,k)
+print(ans)
