37d5913ee8a5e4cb20cf367693fdf20502f5160e
86159 <zxw3180716935@163.com>
2023-01-19 00:00:50 +0800

    add

--- 算法/技能升级.py
+++ 算法/技能升级.py
@@ -27,79 +27,43 @@ upgrade.sort(reverse=True)
 print(sum(upgrade[0:m]))
 '''
 
-'''
-# 二分（有误版）
-n,m = list(map(int,input().split()))
-a = []
-b = []
+# 二分
 
-def decide(mid):
-    num = 0
-    for i in range(n):
-        if mid <= a[i]:
-            num += (a[i]-mid)/b[i]+1
-    return num>=m
-
-for i in range(n):
-    lst = list(map(int,input().split()))
-    a.append(lst[0])
-    b.append(lst[1])
-
-l = 0
-r = 1000000
-
-while(l<r):
-    mid = int((l+r+1)/2) # +1 防止死循环
-    if decide(mid):l = mid
-    else: r = mid - 1
-
-cnt = 0
-res = 0
-for i in range(n):
-    if a[i]>l:
-        c = int((a[i]-l)/b[i]+1)
-        cnt += c
-        end = a[i] - (c-1)*b[i]
-        res += (a[i]+end)*c/2
-
-print(res-(cnt-m)*r)
-'''
-
-# 二分(不知道为啥，就是不对)
-
-n,m = list(map(int,input().split()))
+n, m = list(map(int, input().split()))
 
 aAndb = []
 amax = 0
+
 
 def decide(mid):
     num = 0
     for i in aAndb:
         if mid <= i[0]:
-            num += (i[0]-mid)//i[1]+1
-    return num>=m
+            num += (i[0] - mid) // i[1] + 1
+    return num >= m
+
 
 while n:
     n -= 1
-    ai,bi = list(map(int,input().split()))
-    aAndb.append([ai,bi])
-    amax = max(ai,amax)
+    ai, bi = list(map(int, input().split()))
+    aAndb.append([ai, bi])
+    amax = max(ai, amax)
 
-l,r = 1,amax
-while l<r:
-    mid = (l + r + 1) >> 1   # +1 防止死循环; >> 是右移运算，相当于除以 2 并向下取整，或 (l+r+1)//2
+l, r = 0, amax
+while l < r:
+    mid = (l + r + 1) >> 1  # +1 防止死循环; >> 是右移运算，相当于除以 2 并向下取整，或 (l+r+1)//2
     if decide(mid):
         l = mid
     else:
-        r = mid - 1
+        l = mid - 1
 
 cnt = 0
 res = 0
 for i in aAndb:
-    if i[0]>l:
-        c = (i[0]-l)//i[1]+1
+    if i[0] > l:
+        c = (i[0] - l) // i[1] + 1
         cnt += c
-        end = i[0] - (c-1)*i[1]
-        res += (i[0]+end)*c >> 1
+        end = i[0] - (c - 1) * i[1]
+        res += (i[0] + end) * c >> 1
 
-print(res-(cnt-m)*r)
+print(res - (cnt - m) * l)
