9eb519e2d40dd978ad391a9bc176c405f38047e4
86159 <zxw3180716935@163.com>
2023-02-07 22:58:24 +0800

    add

---
+++ 算法/AlgorithmTemplate/DataStructure/Stack.py
@@ -0,0 +1,39 @@
+values = [0] * 100010
+num = 0
+
+
+def push(x):
+    global num
+    num += 1
+    values[num] = x
+
+
+def pop():
+    global num
+    num -= 1
+
+
+def empty():
+    global num
+    if num == 0:
+        print("YES")
+    else:
+        print("NO")
+
+
+def query():
+    global num
+    print(values[num])
+
+
+M = int(input())
+for i in range(M):
+    lst = input().split()
+    if lst[0] == "push":
+        push(lst[1])
+    elif lst[0] == "query":
+        query()
+    elif lst[0] == "pop":
+        pop()
+    else:
+        empty()
