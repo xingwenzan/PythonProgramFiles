082e2f00fde642c6307bf02a9d9a628e572a424c
86159 <zxw3180716935@163.com>
2023-02-27 16:57:20 +0800

    add

---
+++ 算法/其他题解/截断数组.py
@@ -0,0 +1,19 @@
+# https://www.acwing.com/problem/content/3959/
+
+import 算法.AlgorithmTemplate.BasicAlgorithms.BasicAlgorithmsTemplate as BA
+
+n = int(input())
+lst = list(map(int,input().split()))
+
+prefixAnd = BA.prefix_sum_1D(lst)
+cnt,ans = 0,0
+
+if prefixAnd[n]%3!=0 or n<3:
+    print(0)
+else:
+    for i in range(2,n,1):
+        if prefixAnd[i-1]==prefixAnd[n]/3:
+            cnt+=1
+        if prefixAnd[i]==prefixAnd[n]*2/3:
+            ans+=cnt
+    print(ans)
